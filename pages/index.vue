a35955eeea3425d57a6aa6061c9e9cd0d1d03b12
Syuhei Fujita <syuheifujita0117@gmail.com>
2021-05-27 03:42:45 +0900

    add function modal

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,44 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        view-video-modal
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+      <h3>view-video-modal</h3>
+      <button @click="changeStatusModal()">
+        OPEN MODAL
+      </button>
+
+      <div class="modal" v-if="isOpened" @click.self="changeStatusModal()">
+        <div class="modal-content">
+          <p>modal</p>
+          <!-- <video
+            src="https://www.youtube.com/embed/n5U1zR6SWRE" controls
+            width="300" height="168"
+            v-playing="isOpened"
+          ></video> -->
+          <iframe width="300" height="168" src="https://www.youtube.com/embed/n5U1zR6SWRE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      isOpened: false,
+    }
+  },
+  methods: {
+    changeStatusModal: function() {
+      this.isOpened = !this.isOpened
+    }
+  },
+  // directives: {
+  //   playing(el, binding){
+  //     binding.value ? el.play() : el.pause();
+  //   }
+  // },
+}
 </script>
 
 <style>
@@ -41,33 +51,27 @@ export default {}
   text-align: center;
 }
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+.modal {
+  position: fixed; top: 0; left: 0;
+  width: 100%; height: 100%;
+  z-index: 10;
+  background: rgba(0, 0, 0, .7);
+  transform: translate3d(0, 0, 0);
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+.modal-content {
+  position: absolute; top: 50%; left: 50%;
+  transform: translate(-50%, -50%);
 }
 
-.links {
-  padding-top: 15px;
+.modal-content > p {
+  margin: 0;
+  width: 400px;
+  vertical-align: middle;
+  text-align: center;
+  font: bold 12px/100px sans-serif;
+  letter-spacing: .1em;
+  color: #aaa;
+  background: #fff;
 }
 </style>
